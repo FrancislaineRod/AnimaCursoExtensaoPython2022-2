8ba57f2c4db1adb09c2596a6b1bbafad5d05e279
Francislaine Rodrigues <42657862+FrancislaineRod@users.noreply.github.com>
2022-10-20 00:53:19 +0000

    aprendendo a criar variaveis e exibi-las na tela

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 #
 #print() = comando de saída
 print("Aló Mundo!")
+
+# quando quiser guardar uma String!
+nome = "Francislaine Rodrigues"
+# quando quiser guardar um inteiro!
+idade = 24
+
+#Exibir o meu nome que está dentro da variável nome e idade que está dentro da variável idade.
+print("Meu nome é ",nome, " e minha idade é ",idade,"anos")
 '''
 comentário em bloco
 
