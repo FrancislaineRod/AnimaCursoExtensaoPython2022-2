ccd9a77a7caaf1cacb2726c91a84553473b6e692
Francislaine Rodrigues <42657862+FrancislaineRod@users.noreply.github.com>
2022-10-27 01:06:44 +0000

    exemplo if,elif,else

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-# comando input(): quero permitir que o usuário digite algo...
-nome = input("Digite seu nome: ")
-#pedir idade...
-idade = int(input("Digite a sua idade: "))
-#pedir o gênero
-genero = input("Informe o seu gênero M-masculino, F-feminino, O-outros: ")
+#Pede o nome do aluno e sua nota (de 0 a 10) e, se ele tirou nota 10, mostra "{nome}, Você é bichão, mesmo..."
+nome = input("Informe o seu nome: ")
+nota = float(input("Digite sua nota: "))
 
-#comando de saída...exibir na tela
-print(f"Boa Noite,seu nome é {nome}")
-#mostrar idade...
-print("Sua idade é {}".format(idade))
-#mostrar gênero
-print("Seu gênero é {}".format(genero))
-
-#e se eu quisesse mostrar o DOBRO  da idade informada?
-dobro = idade * 2
-print("O dobro da idade informada é {}".format(dobro))
-
-#estrutura condicional "se"(if)
-#se a pessoa for maior de idade, mostre "você é maior de idade,ótimo!Já pode beber ou dirigir"
-if idade>=18 and genero=="M":
-  print("você é maior de idade,ótimo!Já pode beber ou dirigir e você também precisa/precisou prestar o serviço militar obrigatório")
-elif idade>=18 and (genero=="F" or genero=="O"):
-   print("você é maior de idade,ótimo!Já pode beber ou dirigir")
+if nota==10:
+  print("{}, Você é bichão, mesmo...".format(nome))
+elif nota >= 6 and nota < 10:
+  print("{}, bom trabalho!".format(nome))
 else:
-  print("Você é xóven ainda,xóven ainda...")
-
-
-print("vai ser execultado de qualquer jeito")
+  print("Burro,não tirou dez...")
