3baab79b5b940580ba6a819b7a3f2391031bfb27
Francislaine Rodrigues <42657862+FrancislaineRod@users.noreply.github.com>
2022-11-09 23:22:22 +0000

    exemplo de while e lista com while...

---
+++ aula3-2022-11-09.py
@@ -0,0 +1,49 @@
+print('Início da aula 3(09/11/2023)')
+
+contador = 1
+
+# Exibir de 1 até 10 repetidamente
+while (contador <= 10):
+  print(contador)
+  contador = contador + 1  #contador += 1
+
+#Laço for (pytho for = for each)
+fruta = "morango"
+print(fruta)
+
+fruta1 = "morango"
+fruta2 = "Laranja"
+fruta3 = "pêra"
+
+#Lista
+frutas = ["morango", "Laranja", "pêra"]
+
+#mostra todos
+print(frutas)
+#quero exibir apenas a 3a. fruta(pêra)
+print(frutas[2])
+
+#Exibir quantas frutas tem na minha Lista?
+print(len(frutas))#length = tamanho
+
+#Quero incluir uma fruta nova
+frutas.append("manga")
+
+print(len(frutas))#length = tamanho
+print(frutas)
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+#print(frutas[4])
+
+print("Exemplo das frutas com whie..")
+frutas.append("uva")
+
+i = 0
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
+
+#
