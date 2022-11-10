1c713b1e2bf0a27e0f3f483314a45110eb08a6f2
Francislaine Rodrigues <42657862+FrancislaineRod@users.noreply.github.com>
2022-11-10 01:03:00 +0000

    calcular função+lista

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -13,10 +13,21 @@ print(imposto2)
 #criar uma função calcular_Imposto() que calcula um imposto de 5% e retorna a quem pediu...
 #Isso é a declaração da função(como ela funciona)
 def calcular_Imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 #Aqui é o uso...aqui é o imposto a calcular...e exibir na tela
 preco = 299
 imposto = calcular_Imposto(preco)
-print(imposto)
+print(f"Esse aqui é com a função (7%):{imposto}")
+
+#Eplicação de variavel local x global
+print(preco)
+preco_produto = 100
+print(preco_produto)
+
+#Agora calcular imposto a alíquota agora é 7%
+valores = [1.99, 25.50, 78.27, 1515.5]
+#se eu quiser calcular o iposto destes quatro valores... e exibir na tela assim: "O imposto de ....é..."(1°. preço, 2°. imposto)
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_Imposto(valor)}")
